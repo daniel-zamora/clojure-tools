ac5634d594f75f4472af9e5a3f81470cafb9c219
daniel-zamora <daniel.p.zamora@nasa.gov>
2025-07-14 12:24:44 -0400

    adds license

--- project.clj
+++ project.clj
@@ -1,5 +1,7 @@
 (defproject org.clojars.daniel-zamora/clojure-tools "1.1.5-SNAPSHOT"
   :description "Clojure tools is a set of clojure utilities pulled out of Conjure."
+  :license {:name "Apache License, Version 2.0"
+            :url "http://www.apache.org/licenses/LICENSE-2.0"}
   :dependencies [[org.apache.commons/commons-lang3 "3.18.0"]
                  [org.clojure/clojure "1.4.0"]
                  [org.clojure/java.classpath "0.2.1"]
